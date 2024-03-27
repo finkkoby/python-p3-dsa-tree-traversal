1cee1825006f2d152e2e0e29a21794ea820d39e6
Koby Fink <finkkoby@icloud.com>
2024-03-27 15:57:05 -0400

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    # Initialize an list of nodes to visit and add the root node to it
+    nodes_to_visit = []
+    nodes_to_visit.append(self.root)
+    # While there are nodes in the list of nodes to visit
+    while len(nodes_to_visit) > 0:
+      target = nodes_to_visit.pop(0)
+    #   Remove the first node from the list of nodes to visit
+      if target['id'] == id:
+        return target
+      else:
+        nodes_to_visit += target["children"]
